f809eab0029122bbcdc62e4d786be5587cebab0e
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2015-11-20 12:19:03 -0600

    commit friday's class work

---
+++ Jitter/Models/JitterContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Jitter.Models
+{
+    public class JitterContext : DbContext
+    {
+        // IDbset, IQueryable, IList
+        public DbSet<JitterUser> JitterUsers { get; set; }
+        public DbSet<Jot> Jots { get; set;}
+    }
+}
